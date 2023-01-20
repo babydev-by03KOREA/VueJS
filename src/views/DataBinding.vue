f56020a41b284f039a36c7a360bd2fb82d844cb1
박형주 <hyeongju1122@gmail.com>
2023-01-21 02:05:04 +0900

    v-model + snipet

---
+++ src/views/DataBinding.vue
@@ -0,0 +1,41 @@
+<!-- DATA BINDING -->
+<template>
+  <div>
+    <!-- 문자열로 인식함! -->
+    <div>{{ htmlString }}</div>
+    <!-- 실제 HTML 출력을 위해선 v-html 사용! -->
+    <div v-html="htmlString"></div>
+  </div>
+  <div>
+    <input type="text" v-model="valueModel" /><br>
+    <input type="number" v-model.number="numberModel"/>
+  </div>
+  <div>
+    <textarea v-model="areaMessage"></textarea>
+  </div>
+  <div>
+    <select v-model="city">
+      <option value="01">東京</option>
+      <option value="02">大阪</option>
+      <option value="03">福岡</option>
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      htmlString: '<p id="tomato">This is a tomato string.</p>',
+      valueModel: 'South Korea',
+      numberModel: 2420001,
+      areaMessage: '離したいのがあるならすぐ　GO!',
+      city: '03',
+    };
+  },
+}
+</script>
+<style>
+#tomato {
+  color: tomato;
+}
+</style>
